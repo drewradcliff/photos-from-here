c1d10c2593ca89492b7103f8a18ae9076b91229a
Drew Radcliff <sirampr@gmail.com>
2020-01-21 14:49:12 -0500

    add functionality to index through images

--- script.js
+++ script.js
@@ -4,15 +4,18 @@
 // [x] Use fetch to send the request to Flickr
 // [x] Process the response data into an object
 // [x] Use the values in the response object to construct an image source URL
-// [] Display the first image on the page
+// [x] Display the first image on the page
 // [] In response to some event (e.g. a button click or a setInterval), show the next image in the collection
 
 
 function init () {
+    const status = document.querySelector('.status');
+    let json = {}, imgIndex=0;
 
     function success(position) {
         const latitude = position.coords.latitude;
         const longitude = position.coords.longitude;
+        status.textContent = `Current Location: ${latitude}, ${longitude}`;
 
         getJson(latitude, longitude)
     }
@@ -21,25 +24,28 @@ function init () {
         // default location set to Indianapolis
         const longitude = '39.76574';
         const latitude = '-86.1579024';
+        status.textContent = `Could not get location... Showing default location: ${latitude}, ${longitude}`;
 
         getJson(latitude, longitude)
     }
     
     function getJson (lat, lon) {
-        fetch('https://shrouded-mountain-15003.herokuapp.com/https://flickr.com/services/rest/?api_key=f28f6f6111a311294dc988ab32e57546&format=json&nojsoncallback=1&method=flickr.photos.search&safe_search=1&per_page=5&lat=' + lat + '&lon=' + lon + '&text=city')
+        fetch('https://shrouded-mountain-15003.herokuapp.com/https://flickr.com/services/rest/?api_key=f28f6f6111a311294dc988ab32e57546&format=json&nojsoncallback=1&method=flickr.photos.search&safe_search=1&per_page=5&lat=' + lat + '&lon=' + lon + '&text=street')
         .then((response) => {
             return response.json();
         })
         .then((data) => {
+            json = data;
             const imageUrl = constructImageURL(data.photos.photo[0]);
-            displayImage(imageUrl)
+            displayImage(imageUrl);
         });
     }
     
     function displayImage (url) {
-        let img = document.createElement('img');
-        img.src = url
-        document.body.appendChild(img)
+        let img = document.querySelector('.image');
+        img.src = url;
+        let div = document.querySelector('.image-wrapper');
+        div.appendChild(img);
     }
 
     function constructImageURL (photoObj) {
@@ -48,7 +54,23 @@ function init () {
                 "/" + photoObj.id + "_" + photoObj.secret + ".jpg";
     }
 
+    function prevImg () {
+        if (imgIndex > 0) {
+            imgIndex--;
+            displayImage(constructImageURL(json.photos.photo[imgIndex]));
+        }
+    }
+
+    function nextImg () {
+        if (imgIndex < json.photos.photo.length-1) {
+            imgIndex++;
+            displayImage(constructImageURL(json.photos.photo[imgIndex]))
+        }
+    }
+
     navigator.geolocation.getCurrentPosition(success, error);
+    document.querySelector('.previous').addEventListener('click', prevImg);
+    document.querySelector('.next').addEventListener('click', nextImg);
 }   
 
 init()
